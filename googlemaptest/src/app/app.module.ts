b5435f787b57b73b977fa1b051307a5cdcbe0241
roxas77 <genesigzero@naver.com>
2018-05-23 13:11:09 +0900

    구글맵테스트

---
+++ googlemaptest/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+import { AgmCoreModule } from '@agm/core';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDoPZbZHhzVls-uE9MN2kEKL_2RqhjznTo'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
