89b1f2bec276997a22af89eca4bf10d7fe4fb420
roxas77 <genesigzero@naver.com>
2018-09-20 17:05:56 +0900

    가입 기능 작성

--- angular-site/src/app/app-routing.module.ts
+++ angular-site/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { Error404Component } from './error404/error404.component';
 import { LoginComponent } from './login/login.component';
+import { UserNewComponent } from './user-new/user-new.component';
 
 const routes: Routes = [
   { path: '',  component: WelcomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: '**', component: Error404Component }, //1
+  { path: 'users/new',  component: UserNewComponent },
+  { path: '**', component: Error404Component },
 ];
 
 @NgModule({
--- angular-site/src/app/app.module.ts
+++ angular-site/src/app/app.module.ts
@@ -1,35 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'; //*
-import { HttpClientModule } from '@angular/common/http';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule }    from './app-routing.module';
 
 import { UtilService } from './util.service';
 import { AuthService } from './auth.service';
 import { RequestInterceptor } from './request-interceptor.service';
+import { UserService } from './user.service'; //*
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { Error404Component } from './error404/error404.component';
-import { LoginComponent } from './login/login.component'; //*
+import { LoginComponent } from './login/login.component';
+import { UserNewComponent } from './user-new/user-new.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     Error404Component,
-    LoginComponent, //*
+    LoginComponent,
+    UserNewComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule, //*
-    ReactiveFormsModule, //*
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [ //*
+  providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: RequestInterceptor,
@@ -37,6 +39,7 @@ import { LoginComponent } from './login/login.component'; //*
     },
     UtilService,
     AuthService,
+    UserService,
   ],
   bootstrap: [AppComponent]
 })
